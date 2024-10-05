a3278e2fbd8daf2354ec856eab1e9c1cdec4e25c
ullanor <pawel.nowacki95@gmail.com>
2024-10-05 08:12:16 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void  main(String[] args){
+        System.out.println("\nHello world!");
+    }
+}
+
+/*
+        long startTime = System.currentTimeMillis();
+        System.out.println(startTime);
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 15; i++) {
+
+            System.out.println("i = " + i);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(System.currentTimeMillis()-startTime);
+        sb.append("ms");
+        System.out.println(sb);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter name 1");
+        System.out.println(scanner.nextLine());
+
+ */
